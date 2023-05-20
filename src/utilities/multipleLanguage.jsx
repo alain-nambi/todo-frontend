a5cc2e98f414f841780476dc3ba526999cc49591
Alain <alainnambi@gmail.com>
2023-05-20 11:06:32 +0300

    Update multipleLanguage modules : * Add multiple languages for span's button

--- src/utilities/multipleLanguage.jsx
+++ src/utilities/multipleLanguage.jsx
@@ -4,6 +4,13 @@ let globalLanguages = {
             fr: "Ajouter une tâche",
             en: "Add new task",
             mg: "Anampy asa vaovao"
+        },
+        button: {
+            span: {
+                fr: "Créer",
+                en: "Create",
+                mg: "Anampy"
+            }
         }
     }
 }
