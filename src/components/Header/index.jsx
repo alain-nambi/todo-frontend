399f9fcd0b62b7b2d8a1a57113f63e35e40c6434
Alain <alainnambi@gmail.com>
2023-05-20 09:29:41 +0300

    Update header components * import useState, useEffect from React * import TaskLogo * import styles from header.modules.css * import globalLanguages modules * setPlaceholderLang to english lang by default * add select option for handling language's change

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+
+import TaskLogo from "../../assets/task-deadline-4721444-3926032.webp"
+import styles from "./header.module.css"
+import globalLanguages from '../../utilities/multipleLanguage';
 
 export function Header() {
+  const [placeholderLang, setPlaceholderLang] = useState(globalLanguages.form.placeholder.en)
+
+  const CURRENT_LANG = localStorage.getItem("currentLang")
+
+  useEffect(() => {
+    CURRENT_LANG === "en" && setPlaceholderLang(globalLanguages.form.placeholder.en)
+    CURRENT_LANG === "fr" && setPlaceholderLang(globalLanguages.form.placeholder.fr)
+    CURRENT_LANG === "mg" && setPlaceholderLang(globalLanguages.form.placeholder.mg)
+  }, [])
+
+  function handleChangeLanguage(event) {
+    localStorage.setItem("currentLang", event.target.value)
+
+    event.target.value === "en" && setPlaceholderLang(globalLanguages.form.placeholder.en)
+    event.target.value === "fr" && setPlaceholderLang(globalLanguages.form.placeholder.fr)
+    event.target.value === "mg" && setPlaceholderLang(globalLanguages.form.placeholder.mg)
+  }
+
   return (
-    <>
-      <p>Header</p>
-    </>
+    <header className={styles.header}>
+      <img
+        src={TaskLogo}
+        width={50}
+        alt="TaskLogo"
+      />
+
+      <span>TODO</span>
+
+      <form>
+        <input type="text" placeholder={placeholderLang} />
+      </form>
+
+      <select 
+        name="change-language" 
+        id="changeLanguage" 
+        onChange={handleChangeLanguage}  
+        defaultValue={CURRENT_LANG}
+      >
+        <option value="en">English</option>
+        <option value="fr">Français</option>
+        <option value="mg">Malagasy</option>
+      </select>
+    </header>
   )
 }
