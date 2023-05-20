2b526a5abaaadf95795a0cc8021981918b1957af
Alain <alainnambi@gmail.com>
2023-05-20 11:08:18 +0300

    Update header components : * Add buttonSpanLang state * Convert if statement to switch case statement * Add OnChangeTaskInput and handleSubmit functions

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,49 +5,92 @@ import TaskLogo from "../../assets/task-deadline-4721444-3926032.webp"
 import styles from "./header.module.css"
 import globalLanguages from '../../utilities/multipleLanguage';
 
+import { AiOutlinePlusCircle } from 'react-icons/ai';
+
 export function Header() {
   const [placeholderLang, setPlaceholderLang] = useState(globalLanguages.form.placeholder.en)
+  const [buttonSpanLang, setButtonSpanLang] = useState(globalLanguages.form.button.span.en)
+  const [taskInput, setTaskInput] = useState("")
 
   const CURRENT_LANG = localStorage.getItem("currentLang")
 
   useEffect(() => {
-    CURRENT_LANG === "en" && setPlaceholderLang(globalLanguages.form.placeholder.en)
-    CURRENT_LANG === "fr" && setPlaceholderLang(globalLanguages.form.placeholder.fr)
-    CURRENT_LANG === "mg" && setPlaceholderLang(globalLanguages.form.placeholder.mg)
+    switch (CURRENT_LANG) {
+      case "en":
+        setPlaceholderLang(globalLanguages.form.placeholder.en)
+        setButtonSpanLang(globalLanguages.form.button.span.en)
+        break;
+    
+      case "fr":
+        setPlaceholderLang(globalLanguages.form.placeholder.fr)
+        setButtonSpanLang(globalLanguages.form.button.span.fr)
+        break;
+      
+      case "mg":
+        setPlaceholderLang(globalLanguages.form.placeholder.mg)
+        setButtonSpanLang(globalLanguages.form.button.span.mg)
+    }
   }, [])
 
   function handleChangeLanguage(event) {
     localStorage.setItem("currentLang", event.target.value)
 
-    event.target.value === "en" && setPlaceholderLang(globalLanguages.form.placeholder.en)
-    event.target.value === "fr" && setPlaceholderLang(globalLanguages.form.placeholder.fr)
-    event.target.value === "mg" && setPlaceholderLang(globalLanguages.form.placeholder.mg)
+    switch (event.target.value) {
+      case "en":
+        setPlaceholderLang(globalLanguages.form.placeholder.en)
+        setButtonSpanLang(globalLanguages.form.button.span.en)
+        break;
+    
+      case "fr":
+        setPlaceholderLang(globalLanguages.form.placeholder.fr)
+        setButtonSpanLang(globalLanguages.form.button.span.fr)
+        break;
+      
+      case "mg":
+        setPlaceholderLang(globalLanguages.form.placeholder.mg)
+        setButtonSpanLang(globalLanguages.form.button.span.mg)
+    }
+  }
+
+  function OnChangeTaskInput(event) {
+    setTaskInput(event.target.value)
+  }
+
+  function handleSubmit (event) {
+    event.preventDefault()
+    setTaskInput("")
   }
 
   return (
     <header className={styles.header}>
-      <img
-        src={TaskLogo}
-        width={50}
-        alt="TaskLogo"
-      />
+      <div className={styles.headerContent}>
+        <img
+          src={TaskLogo}
+          width={50}
+          alt="TaskLogo"
+        />
 
-      <span>TODO</span>
+        <h1 className={styles.title}>todo</h1>
+      </div>
 
-      <form>
-        <input type="text" placeholder={placeholderLang} />
+      <form onSubmit={handleSubmit} className={styles.taskForm}>
+        <select 
+          name="change-language" 
+          id="changeLanguage" 
+          onChange={handleChangeLanguage}  
+          defaultValue={CURRENT_LANG}
+        >
+          <option value="en">EN</option>
+          <option value="fr">FR</option>
+          <option value="mg">MG</option>
+        </select>
+
+        <input type="text" placeholder={placeholderLang} onChange={OnChangeTaskInput} value={taskInput} />
+
+        <button>{buttonSpanLang} <AiOutlinePlusCircle size={20}/></button>
       </form>
 
-      <select 
-        name="change-language" 
-        id="changeLanguage" 
-        onChange={handleChangeLanguage}  
-        defaultValue={CURRENT_LANG}
-      >
-        <option value="en">English</option>
-        <option value="fr">Français</option>
-        <option value="mg">Malagasy</option>
-      </select>
+     
     </header>
   )
 }
