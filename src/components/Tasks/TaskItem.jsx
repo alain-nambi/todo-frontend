9d82add7ba19667c12e0403451cd1b7b92b779cb
alain-nambi <alainnambi@gmail.com>
2023-06-27 16:25:05 +0300

    feat: Add delete task modal

--- src/components/Tasks/TaskItem.jsx
+++ src/components/Tasks/TaskItem.jsx
@@ -1,45 +1,58 @@
-import React from "react";
+import { React, useState } from "react";
 
 // Importing necessary dependencies
 import styles from "./tasks.module.css";
 import { Checkbox, Button } from "@material-tailwind/react";
 import { CiEdit, CiTrash } from "react-icons/ci";
+import { DeleteTaskModal } from "../Modal/Modal";
 
 export const TaskItem = ({ task, onDelete }) => {
+  const [openDeleteTaskModal, setOpenDeleteTaskModal] = useState(false);
+  const handleOpenDeleteTaskModal = () =>
+    setOpenDeleteTaskModal(!openDeleteTaskModal);
+
   return (
-    <li key={task.id} className="mb-2">
-      <div
-        className={` ${styles.taskList} mb-1 flex items-center justify-between rounded-md cursor-pointer`}
-      >
-        <Checkbox color="blue" />
+    <>
+      <DeleteTaskModal
+        task={task}
+        openDeleteTaskModal={openDeleteTaskModal}
+        handleOpenDeleteTaskModal={handleOpenDeleteTaskModal}
+        onDelete={onDelete}
+      />
+      <li key={task.id} className="mb-2">
+        <div
+          className={` ${styles.taskList} mb-1 flex items-center justify-between rounded-md cursor-pointer`}
+        >
+          <Checkbox color="blue" />
 
-        {/* Displaying task title */}
-        <span>{task.title}</span>
+          {/* Displaying task title */}
+          <span>{task.title}</span>
 
-        {/* Adding gap between buttons */}
-        <div className="flex items-center gap-2">
-          {/* Adding Edit button */}
-          <Button
-            variant="gradient"
-            title="Modifier"
-            className="p-2"
-            color="blue"
-          >
-            <CiEdit size={20} />
-          </Button>
+          {/* Adding gap between buttons */}
+          <div className="flex items-center gap-2">
+            {/* Adding Edit button */}
+            <Button
+              variant="gradient"
+              title="Modifier"
+              className="p-2"
+              color="blue"
+            >
+              <CiEdit size={20} />
+            </Button>
 
-          {/* Adding Delete button */}
-          <Button
-            variant="gradient"
-            title="Supprimer"
-            className="p-2"
-            color="red"
-            onClick={() => onDelete(task.id)}
-          >
-            <CiTrash size={20} />
-          </Button>
+            {/* Adding Delete button */}
+            <Button
+              variant="gradient"
+              title="Supprimer"
+              className="p-2"
+              color="red"
+              onClick={handleOpenDeleteTaskModal}
+            >
+              <CiTrash size={20} />
+            </Button>
+          </div>
         </div>
-      </div>
-    </li>
+      </li>
+    </>
   );
 };
